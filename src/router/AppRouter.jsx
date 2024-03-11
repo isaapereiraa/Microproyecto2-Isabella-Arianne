18c92e82d2592b97fc5a62dd851096955328ef6d
isaapereiraa <isabella.pereira@correo.unimet.edu.ve>
2024-03-11 11:48:59 -0400

    Login

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "../Navbar";
+import { Home, Login, Registro, Dashboard } from "../pages";
+import { PrivateRoute } from "./PrivateRoute";
+
+export const AppRouter = () => {
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Navbar />}>
+					<Route index element={<Home />} />
+					<Route path='login' element={<Login />} />
+					<Route path='registro' element={<Registro />} />
+					<Route
+						path='dashboard'
+						element={
+							<PrivateRoute>
+								<Dashboard />
+							</PrivateRoute>
+						}
+					/>
+				</Route>
+			</Routes>
+		</>
+	);
+};
