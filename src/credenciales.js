ff207b4da423a796c7b0ba0429c96de23533c462
isaapereiraa <isabella.pereira@correo.unimet.edu.ve>
2024-03-11 14:20:00 -0400

    credenciales.js

---
+++ src/credenciales.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCZHMh8QRVPVD1zJBlfrwN9fUnxp11Yf4",
+  authDomain: "metro-juegos.firebaseapp.com",
+  projectId: "metro-juegos",
+  storageBucket: "metro-juegos.appspot.com",
+  messagingSenderId: "984399436114",
+  appId: "1:984399436114:web:ec80dc13491ddb41e6428b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
